9079e92c10cdb48eaf5297d0751e61c06b2df7fc
AndrewRagnarNicholas <andrew.danger.nicholas@gmaoil.com>
2015-11-18 16:15:43 -0500

    Fix1
    Removed bad calls and corrected binary flags

--- purgeLocalItems.sh
+++ purgeLocalItems.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 currentUser=`ls -l /dev/console | cut -d " " -f 4`
 UUID=`system_profiler SPHardwareDataType | grep 'Hardware UUID' | awk '{print $3}'`
-currentHome="/Users/$currentUser/"
-localItems="$currentHome/Keychains/$UUID"
+currentHome="/Users/$currentUser"
+localItems="$currentHome/Library/Keychains"
+badPath="$currentHome/Library/Keychains/BadLocalItems"
 
 if [ -d $localItems ]; then
-	echo "Creating BadLocalItems Folder"
-	mkdir -p "$currentHome/Keychains/BadLocalItems"
-	echo "Moving LOCKED local Items"
-	mv -fr $localItems "$currentHome/Keychains/BadLocalItems-`date +%Y%m%d%H%M`"
-	echo "Locked local items moved"
+        echo "Creating BadLocalItems Folder"
+        mkdir -p "$badPath"
+        echo "Moving LOCKED local Items"
+        mv -f $localItems "$badPath/$UUID-`date +%Y%m%d%H%M`"
+        echo "Locked local items moved"
 else
-	echo "LocalItems Keychain has already been moved or deleted. Machine restart needed"
+        echo "LocalItems Keychain has already been moved or deleted. Machine restart needed"
 fi
+
